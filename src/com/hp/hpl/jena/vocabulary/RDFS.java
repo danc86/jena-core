38c51bcf77b3fdee625d567043ecd8d3a6b5c548
Ian Dickinson <i.j.dickinson@gmail.com>
2003-03-03 22:36:04 +0000

    Added missing resource for rdfs:Datatype

--- src/com/hp/hpl/jena/vocabulary/RDFS.java
+++ src/com/hp/hpl/jena/vocabulary/RDFS.java
@@ -50,6 +50,8 @@ public class RDFS {
     
            static final String   nClass    = "Class";
     public static       Resource Class     = null;
+           static final String   nDatatype           = "Datatype";
+    public static       Resource Datatype            = null;
            static final String   nConstraintProperty = "ConstraintProperty";
     public static       Resource ConstraintProperty  = null;
            static final String   nContainer          = "Container";
@@ -88,6 +90,7 @@ public class RDFS {
     static {
         try {
             Class = new ResourceImpl(uri+nClass);
+            Datatype = new ResourceImpl(uri+nDatatype);
             ConstraintProperty = new ResourceImpl(uri+nConstraintProperty);
             Container = new ResourceImpl(nContainer);
             ContainerMembershipProperty = 
