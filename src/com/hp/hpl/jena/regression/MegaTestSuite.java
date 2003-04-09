2e310549f6e967eeff2d6c11a7a65c78bd493fb9
Chris Dollin <ehog.hedge@googlemail.com>
2003-04-09 09:30:01 +0000

    Removed "DAML tests suppressed" code from MegaTests. build.xml now copies the vocabularies directory. Updated readme to not mention the "DAML tests" message.

--- src/com/hp/hpl/jena/regression/MegaTestSuite.java
+++ src/com/hp/hpl/jena/regression/MegaTestSuite.java
@@ -70,10 +70,10 @@ public class MegaTestSuite extends TestSuite {
             "inference test",
             com.hp.hpl.jena.inference.InferenceTestSuite.suite());
             */
-        if (false)
-            addTest("DAML", com.hp.hpl.jena.ontology.daml.test.DAMLTest.suite());
-        else
-            System.err.println("WARNING: DAML tests suppressed for the moment");
+//         if (false)
+//             addTest("DAML", com.hp.hpl.jena.ontology.daml.test.DAMLTest.suite());
+//         else
+//             System.err.println("WARNING: DAML tests suppressed for the moment");
     }
     private void addTest(String name, TestSuite tc) {
         tc.setName(name);
