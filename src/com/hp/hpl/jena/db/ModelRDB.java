d4d1ab9ad360af4e94f7f6c19af8b3aea7859de1
wkw <wkw>
2003-07-15 15:25:35 +0000

    javadoc patch for createModel.

--- src/com/hp/hpl/jena/db/ModelRDB.java
+++ src/com/hp/hpl/jena/db/ModelRDB.java
@@ -137,7 +137,7 @@ public class ModelRDB extends ModelCom implements Model, ModelI {
 	 * getDefaultModelProperties, modify it, and then use that as an argument
 	 * in the call.
 	 * 
-    * @param name the name to give the newly created model
+     * @param dbcon a DBConnectionI specifying the database connection
      * @param modelProperties a Model containing customization properties
      * @since Jena 2.0
      */
