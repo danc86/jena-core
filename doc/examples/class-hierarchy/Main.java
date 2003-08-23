7005cd5609df3ea810cb4dcd702b4c30259cfc34
Ian Dickinson <i.j.dickinson@gmail.com>
2003-08-23 00:10:40 +0000

    Added two new ontology example programs

---
+++ doc/examples/class-hierarchy/Main.java
@@ -0,0 +1,77 @@
+/*****************************************************************************
+ * Source code information
+ * -----------------------
+ * Original author    Ian Dickinson, HP Labs Bristol
+ * Author email       ian.dickinson@hp.com
+ * Package            Jena 2
+ * Web                http://sourceforge.net/projects/jena/
+ * Created            22-Aug-2003
+ * Filename           $RCSfile$
+ * Revision           $Revision$
+ * Release status     $State$
+ *
+ * Last modified on   $Date$
+ *               by   $Author$
+ *
+ *****************************************************************************/
+
+// Package
+///////////////
+
+// Imports
+///////////////
+import com.hp.hpl.jena.ontology.*;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+
+
+/**
+ * <p>
+ * Execution wrapper for class hierarchy example
+ * </p>
+ *
+ * @author Ian Dickinson, HP Labs
+ *         (<a  href="mailto:Ian.Dickinson@hp.com" >email</a>)
+ * @version CVS $Id$
+ */
+public class Main {
+    // Constants
+    //////////////////////////////////
+
+    // Static variables
+    //////////////////////////////////
+
+    // Instance variables
+    //////////////////////////////////
+
+    // Constructors
+    //////////////////////////////////
+
+    // External signature methods
+    //////////////////////////////////
+    
+    public static void main( String[] args ) {
+        OntModel m = ModelFactory.createOntologyModel( OntModelSpec.OWL_MEM, null );
+        
+        // we have a local copy of the wine ontology
+        m.getDocumentManager().addAltEntry( "http://www.w3.org/2001/sw/WebOnt/guide-src/wine", 
+                                            "file:testing/reasoners/bugs/wine.owl" );
+        m.getDocumentManager().addAltEntry( "http://www.w3.org/2001/sw/WebOnt/guide-src/food", 
+                                            "file:testing/reasoners/bugs/food.owl" );
+        m.getDocumentManager().addAltEntry( "http://www.w3.org/2001/sw/WebOnt/guide-src/food.owl", 
+                                            "file:testing/reasoners/bugs/food.owl" );
+
+        m.read( "http://www.w3.org/2001/sw/WebOnt/guide-src/wine" );
+
+        new ClassHierarchy().showHierarchy( System.out, m );
+    }
+
+
+    // Internal implementation methods
+    //////////////////////////////////
+
+    //==============================================================================
+    // Inner class definitions
+    //==============================================================================
+
+}
+
