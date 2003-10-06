1239d0e8ca783011d478412d0f005aa517652edc
Chris Dollin <ehog.hedge@googlemail.com>
2003-10-06 13:41:14 +0000

    Updated release nots for developer zip; added new ExpressionSet for query handling.

--- src/com/hp/hpl/jena/graph/query/ExpressionSet.java
+++ src/com/hp/hpl/jena/graph/query/ExpressionSet.java
@@ -7,7 +7,7 @@
 package com.hp.hpl.jena.graph.query;
 
 /**
-	ExpressionSet
+	ExpressionSet: represent a set of (boolean) expressions ANDed together.
 
 	@author kers
 */
@@ -15,15 +15,31 @@ public class ExpressionSet
     {
     private Expression x = Expression.TRUE;
 
+    /**
+        Initialise an expression set with no members.
+    */
 	public ExpressionSet() 
         {}
 
+    /**
+        Answer this expressionset after e has been anded into it.
+     	@param e the expression to and into the set
+     	@return this ExpressionSet
+    */
     public ExpressionSet add( Expression e )
         {
         x = x.and( e );
         return this;    
         }
+    
+    /**
+        Evaluate this expression set, delivering true iff no member of the set evaluates
+        to false.
         
+     	@param map the binding map to pass to the sub-expressions
+     	@param d the bound variable values to pass to the sub-expressions
+     	@return true iff no member evaluates to false
+     */    
     public boolean evalBool( Mapping map, Domain d )
         {
         return x.evalBool( map, d );    
