069f0e86e72c3b90f82de558d2ea20abea2be51b
Ian Dickinson <i.j.dickinson@gmail.com>
2003-11-26 16:36:31 +0000

    Initial checkpoint on DIG interface for Jena. Not yet fully working. This release includes translating OWL (DAML incomplete and untested) to the DIG concept language.

---
+++ src/com/hp/hpl/jena/reasoner/dig/DigReasonerException.java
@@ -0,0 +1,77 @@
+/*****************************************************************************
+ * Source code information
+ * -----------------------
+ * Original author    Ian Dickinson, HP Labs Bristol
+ * Author email       ian.dickinson@hp.com
+ * Package            @package@
+ * Web site           @website@
+ * Created            17-Nov-2003
+ * Filename           $RCSfile$
+ * Revision           $Revision$
+ * Release status     $State$
+ *
+ * Last modified on   $Date$
+ *               by   $Author$
+ *
+ * @copyright@
+ *****************************************************************************/
+
+// Package
+///////////////
+package com.hp.hpl.jena.reasoner.dig;
+
+
+
+// Imports
+///////////////
+import com.hp.hpl.jena.shared.JenaException;
+
+
+/**
+ * <p>
+ * Exception indicating that a problem has arisen in the interface to the DIG
+ * reasoner.
+ * </p>
+ *
+ * @author Ian Dickinson, HP Labs (<a href="mailto:Ian.Dickinson@hp.com">email</a>)
+ * @version Release @release@ ($Id$)
+ */
+public class DigReasonerException 
+    extends JenaException
+{
+    // Constants
+    //////////////////////////////////
+
+    // Static variables
+    //////////////////////////////////
+
+    // Instance variables
+    //////////////////////////////////
+
+    // Constructors
+    //////////////////////////////////
+
+    /** 
+     * Create a new exception with the given message 
+     */
+    public DigReasonerException( String message ) {
+        super( message );
+    }
+    
+    
+    // External signature methods
+    //////////////////////////////////
+
+    // Internal implementation methods
+    //////////////////////////////////
+
+    //==============================================================================
+    // Inner class definitions
+    //==============================================================================
+
+}
+
+
+/*
+@footer@
+*/
