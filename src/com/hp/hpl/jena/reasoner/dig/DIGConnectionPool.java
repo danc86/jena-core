68d90739fd34e4cc75b4abce8daf20b51005d3fe
Ian Dickinson <i.j.dickinson@gmail.com>
2003-12-02 10:02:54 +0000

    Updated copyright notices

--- src/com/hp/hpl/jena/reasoner/dig/DIGConnectionPool.java
+++ src/com/hp/hpl/jena/reasoner/dig/DIGConnectionPool.java
@@ -3,9 +3,9 @@
  * -----------------------
  * Original author    Ian Dickinson, HP Labs Bristol
  * Author email       ian.dickinson@hp.com
- * Package            @package@
- * Web site           @website@
- * Created            28-Nov-2003
+ * Package            Jena 2
+ * Web                http://sourceforge.net/projects/jena/
+ * Created            11-Sep-2003
  * Filename           $RCSfile$
  * Revision           $Revision$
  * Release status     $State$
@@ -13,8 +13,10 @@
  * Last modified on   $Date$
  *               by   $Author$
  *
- * @copyright@
+ * (c) Copyright 2002-2003, Hewlett-Packard Company, all rights reserved.
+ * (see footer for full conditions)
  *****************************************************************************/
+
 
 // Package
 ///////////////
@@ -101,5 +103,31 @@ public class DIGConnectionPool {
 
 
 /*
-@footer@
+    (c) Copyright Hewlett-Packard Company 2002-2003
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions
+    are met:
+
+    1. Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in the
+       documentation and/or other materials provided with the distribution.
+
+    3. The name of the author may not be used to endorse or promote products
+       derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+    IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+    OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+    IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+    NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+    THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
