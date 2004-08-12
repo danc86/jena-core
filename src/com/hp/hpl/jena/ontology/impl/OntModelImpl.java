459831a6fdad77595140c44f15536b92b3f5480d
Ian Dickinson <i.j.dickinson@gmail.com>
2004-08-12 14:43:16 +0000

    Removed redundant imports to get rid of yellow flag warning in Eclipse

--- src/com/hp/hpl/jena/ontology/impl/OntModelImpl.java
+++ src/com/hp/hpl/jena/ontology/impl/OntModelImpl.java
@@ -29,8 +29,6 @@ import com.hp.hpl.jena.rdf.model.*;
 import com.hp.hpl.jena.rdf.model.impl.*;
 import com.hp.hpl.jena.reasoner.*;
 import com.hp.hpl.jena.reasoner.rulesys.BasicForwardRuleInfGraph;
-import com.hp.hpl.jena.reasoner.rulesys.RDFSRuleInfGraph;
-import com.hp.hpl.jena.reasoner.rulesys.RDFSRuleReasoner;
 import com.hp.hpl.jena.shared.JenaException;
 import com.hp.hpl.jena.util.iterator.*;
 import com.hp.hpl.jena.vocabulary.*;
