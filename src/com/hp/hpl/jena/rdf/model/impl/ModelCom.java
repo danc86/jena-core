35a65d1dd19afcb3bd4dc1806bc0aa1ed796a2f1
Chris Dollin <ehog.hedge@googlemail.com>
2004-12-08 14:11:15 +0000

    Ensured stream opened by the new Model::read(String x 3) method is closed on termination.

--- src/com/hp/hpl/jena/rdf/model/impl/ModelCom.java
+++ src/com/hp/hpl/jena/rdf/model/impl/ModelCom.java
@@ -205,7 +205,12 @@ public class ModelCom
     
     public Model read( String url, String base, String lang )
         {
-        try { read( new URL( url ) .openStream(), base, lang ); }
+        try 
+            { 
+            InputStream is = new URL( url ) .openStream();
+            try { read( is, base, lang ); }
+            finally { is.close(); }
+            }
         catch (IOException e) { throw new WrappedIOException( e ); }
         return this;
         }
