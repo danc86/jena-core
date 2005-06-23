90dc78de6aa814eac76a5a560226c2ba0fcf7f0e
Ian Dickinson <i.j.dickinson@gmail.com>
2005-06-23 22:53:29 +0000

    Tidied up ontology examples: moved code to doc/src-examples, corrected deprecated method calls; tidied and improved code comments.

--- doc/src-examples/jena/examples/ontology/classHierarchy/Main.java
+++ doc/src-examples/jena/examples/ontology/classHierarchy/Main.java
@@ -13,10 +13,14 @@
  * Last modified on   $Date$
  *               by   $Author$
  *
+ * (c) Copyright 2002, 2003, 2004, 2005 Hewlett-Packard Development Company, LP
+ * (see footer for full conditions)
  *****************************************************************************/
 
 // Package
 ///////////////
+package jena.examples.ontology.classHierarchy;
+
 
 // Imports
 ///////////////
@@ -48,14 +52,14 @@ public class Main {
 
     // External signature methods
     //////////////////////////////////
-    
+
     public static void main( String[] args ) {
         OntModel m = ModelFactory.createOntologyModel( OntModelSpec.OWL_MEM, null );
-        
+
         // we have a local copy of the wine ontology
-        m.getDocumentManager().addAltEntry( "http://www.w3.org/2001/sw/WebOnt/guide-src/wine", 
+        m.getDocumentManager().addAltEntry( "http://www.w3.org/2001/sw/WebOnt/guide-src/wine",
                                             "file:testing/reasoners/bugs/wine.owl" );
-        m.getDocumentManager().addAltEntry( "http://www.w3.org/2001/sw/WebOnt/guide-src/food", 
+        m.getDocumentManager().addAltEntry( "http://www.w3.org/2001/sw/WebOnt/guide-src/food",
                                             "file:testing/reasoners/bugs/food.owl" );
 
         m.read( "http://www.w3.org/2001/sw/WebOnt/guide-src/wine" );
@@ -72,4 +76,35 @@ public class Main {
     //==============================================================================
 
 }
+
+
+/*
+    (c) Copyright 2002, 2003, 2004, 2005 Hewlett-Packard Development Company, LP
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions
+    are met:
+
+    1. Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in the
+       documentation and/or other materials provided with the distribution.
+
+    3. The name of the author may not be used to endorse or promote products
+       derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+    IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+    OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+    IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+    NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+    THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 
