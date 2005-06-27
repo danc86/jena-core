79ef6e4139f1b23b5bfaf34bd3acb0948d6f0139
Chris Dollin <ehog.hedge@googlemail.com>
2005-06-27 15:00:14 +0000

    extended TestCase so it's a propery JUnit suitey thingy

--- src/com/hp/hpl/jena/graph/query/test/TestGraphQueryPackage.java
+++ src/com/hp/hpl/jena/graph/query/test/TestGraphQueryPackage.java
@@ -14,7 +14,7 @@ import junit.framework.*;
 /**
  	@author kers
 */
-public class TestGraphQueryPackage
+public class TestGraphQueryPackage extends TestCase
     {
     public TestGraphQueryPackage()
         {}
