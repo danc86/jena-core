714436c3d795fb724af4575ed25e76a638d51217
Chris Dollin <ehog.hedge@googlemail.com>
2005-10-20 08:21:16 +0000

    finished new Alt regression tests

--- src/com/hp/hpl/jena/regression/NewRegressionBagMethods.java
+++ src/com/hp/hpl/jena/regression/NewRegressionBagMethods.java
@@ -24,59 +24,6 @@ public class NewRegressionBagMethods extends NewRegressionContainerMethods
        
     protected Resource getContainerType()
         { return RDF.Bag; }
-  
-    public void testContainerOfIntegers()
-        {
-        int num = 10;
-        Container c = createContainer();
-        for (int i = 0; i < num; i += 1) c.add( i );
-        assertEquals( num, c.size() );
-        NodeIterator it = c.iterator();
-        for (int i = 0; i < num; i += 1)
-            assertEquals( i, ((Literal) it.nextNode()).getInt() );
-        assertFalse( it.hasNext() );
-        }
-    
-    public void testContainerOfIntegersRemovingA()
-        {
-        boolean[] retain = { true,  true,  true,  false, false, false, false, false, true,  true };
-        testContainerOfIntegersWithRemoving( retain );
-        }    
-    
-    public void testContainerOfIntegersRemovingB()
-        {
-        boolean[] retain = { false, true, true, false, false, false, false, false, true, false };
-        testContainerOfIntegersWithRemoving( retain );
-        }    
-    
-    public void testContainerOfIntegersRemovingC()
-        {
-        boolean[] retain = { false, false, false, false, false, false, false, false, false, false };
-        testContainerOfIntegersWithRemoving( retain );
-        }
-
-    protected void testContainerOfIntegersWithRemoving( boolean[] retain )
-        {
-        final int num = retain.length;
-        boolean [] found = new boolean[num];
-        Container c = createContainer();
-        for (int i = 0; i < num; i += 1) c.add( i );
-        NodeIterator it = c.iterator();
-        for (int i = 0; i < num; i += 1)
-            {
-            it.nextNode();
-            if (retain[i] == false) it.remove();
-            }
-        NodeIterator s = c.iterator();
-        while (s.hasNext())
-            {
-            int v = ((Literal) s.nextNode()).getInt();
-            assertFalse( found[v] );
-            found[v] = true;
-            }
-        for (int i = 0; i < num; i += 1)
-            assertEquals( "element " + i, retain[i], found[i] );
-        }
     }
 
 
--- src/com/hp/hpl/jena/regression/NewRegressionContainerMethods.java
+++ src/com/hp/hpl/jena/regression/NewRegressionContainerMethods.java
@@ -66,6 +66,59 @@ public abstract class NewRegressionContainerMethods extends NewRegressionBase
         c.add( tvLitObj ); assertTrue( c.contains( tvLitObj ) );
         assertEquals( 12, c.size() );
         }
+
+    public void testContainerOfIntegers()
+        {
+        int num = 10;
+        Container c = createContainer();
+        for (int i = 0; i < num; i += 1) c.add( i );
+        assertEquals( num, c.size() );
+        NodeIterator it = c.iterator();
+        for (int i = 0; i < num; i += 1)
+            assertEquals( i, ((Literal) it.nextNode()).getInt() );
+        assertFalse( it.hasNext() );
+        }
+
+    public void testContainerOfIntegersRemovingA()
+        {
+        boolean[] retain = { true,  true,  true,  false, false, false, false, false, true,  true };
+        testContainerOfIntegersWithRemoving( retain );
+        }
+
+    public void testContainerOfIntegersRemovingB()
+        {
+        boolean[] retain = { false, true, true, false, false, false, false, false, true, false };
+        testContainerOfIntegersWithRemoving( retain );
+        }
+
+    public void testContainerOfIntegersRemovingC()
+        {
+        boolean[] retain = { false, false, false, false, false, false, false, false, false, false };
+        testContainerOfIntegersWithRemoving( retain );
+        }
+
+    protected void testContainerOfIntegersWithRemoving( boolean[] retain )
+        {
+        final int num = retain.length;
+        boolean [] found = new boolean[num];
+        Container c = createContainer();
+        for (int i = 0; i < num; i += 1) c.add( i );
+        NodeIterator it = c.iterator();
+        for (int i = 0; i < num; i += 1)
+            {
+            it.nextNode();
+            if (retain[i] == false) it.remove();
+            }
+        NodeIterator s = c.iterator();
+        while (s.hasNext())
+            {
+            int v = ((Literal) s.nextNode()).getInt();
+            assertFalse( found[v] );
+            found[v] = true;
+            }
+        for (int i = 0; i < num; i += 1)
+            assertEquals( "element " + i, retain[i], found[i] );
+        }
     }
 
 
