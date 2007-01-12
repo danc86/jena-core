fa25a25be1f182424d6fe359dcf3b1f5ed0127c0
Chris Dollin <ehog.hedge@googlemail.com>
2007-01-12 15:18:53 +0000

    Deprecated RDFException.

--- src/com/hp/hpl/jena/rdf/model/RDFException.java
+++ src/com/hp/hpl/jena/rdf/model/RDFException.java
@@ -14,6 +14,7 @@ import com.hp.hpl.jena.shared.*;
  * but instead uses particular Jena exceptions. Those exceptions presently
  * subclass RDFException and set the magic numbers, but This Will Change.
  * @author bwm
+ * @deprecated
  * @version $Name$ $Revision$ $Date$
  */
 public class RDFException extends JenaException {
