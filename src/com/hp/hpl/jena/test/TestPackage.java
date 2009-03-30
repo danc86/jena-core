6e62557a319aed556a79b80c720512d7b0e364f4
Andy Seaborne <andy.seaborne@gmail.com>
2009-03-30 13:15:16 +0000

    Jars: Removed:  xml-apis.jar, junit.jar / Added: junit-4.5.jar

--- src/com/hp/hpl/jena/test/TestPackage.java
+++ src/com/hp/hpl/jena/test/TestPackage.java
@@ -6,7 +6,7 @@
 
 package com.hp.hpl.jena.test;
 
-import junit.framework.Test;
+import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 import com.hp.hpl.jena.assembler.test.TestAssemblerPackage;
@@ -18,48 +18,47 @@ import com.hp.hpl.jena.assembler.test.TestAssemblerPackage;
  * rather than in this file.
  * @author  jjc
  */
-public class TestPackage extends TestSuite {
+public class TestPackage extends TestCase{
 
-    static public Test suite() {
-        return  new TestPackage();
-    }
-
-    /** Creates new TestPackage */
-    private TestPackage() {
-        super("Jena");
-        addTest("Enhanced", com.hp.hpl.jena.enhanced.test.TestPackage.suite());
-        addTest("Graph", com.hp.hpl.jena.graph.test.TestPackage.suite());
-        addTest( com.hp.hpl.jena.mem.test.TestMemPackage.suite() );
-        addTest("Model", com.hp.hpl.jena.rdf.model.test.TestPackage.suite());
-        addTest("N3", com.hp.hpl.jena.n3.test.N3TestSuite.suite());
-        addTest("Turtle", com.hp.hpl.jena.n3.turtle.test.TurtleTestSuite.suite()) ;
+    static public TestSuite suite() {
+        TestSuite ts = new TestSuite() ;
+        ts.setName("Jena") ;
+        addTest(ts,  "Enhanced", com.hp.hpl.jena.enhanced.test.TestPackage.suite());
+        addTest(ts,  "Graph", com.hp.hpl.jena.graph.test.TestPackage.suite());
+        addTest(ts,  null, com.hp.hpl.jena.mem.test.TestMemPackage.suite() );
+        addTest(ts,  "Model", com.hp.hpl.jena.rdf.model.test.TestPackage.suite());
+        addTest(ts,  "N3", com.hp.hpl.jena.n3.test.N3TestSuite.suite());
+        addTest(ts,  "Turtle", com.hp.hpl.jena.n3.turtle.test.TurtleTestSuite.suite()) ;
 
         // Avoid a compile time dependency on ARQ.
         {
             TestSuite arqSuite = TestPackageARQ.suite() ;
             if ( arqSuite != null )
-                addTest("ARQ", arqSuite) ;
+                addTest(ts, "ARQ", arqSuite) ;
             else
                 System.err.println("ARQ test suite not run") ;
         }
-        addTest("XML Output", com.hp.hpl.jena.xmloutput.test.TestPackage.suite());
-        addTest("Util", com.hp.hpl.jena.util.test.TestPackage.suite());
-        addTest( com.hp.hpl.jena.util.iterator.test.TestPackage.suite() );
-        addTest("Mega", com.hp.hpl.jena.regression.MegaTestSuite.suite());
-        addTest( "Assembler", TestAssemblerPackage.suite() );
-        addTest( com.hp.hpl.jena.rdf.arp.test.TestPackage.suite());
-        addTest( "Vocabularies", com.hp.hpl.jena.vocabulary.test.TestVocabularies.suite() );
-        addTest( com.hp.hpl.jena.shared.test.TestSharedPackage.suite() );
-        addTest("Reasoners", com.hp.hpl.jena.reasoner.test.TestPackage.suite());
-        addTest("Composed graphs", com.hp.hpl.jena.graph.compose.test.TestPackage.suite() );
-        addTest( "Ontology", com.hp.hpl.jena.ontology.impl.test.TestPackage.suite() );
-        addTest( "cmd line utils", jena.test.TestPackage.suite() );
+        addTest(ts,  "XML Output", com.hp.hpl.jena.xmloutput.test.TestPackage.suite());
+        addTest(ts,  "Util", com.hp.hpl.jena.util.test.TestPackage.suite());
+        addTest(ts,  "Jena iterator", com.hp.hpl.jena.util.iterator.test.TestPackage.suite() );
+        addTest(ts,  "Mega", com.hp.hpl.jena.regression.MegaTestSuite.suite());
+        addTest(ts,  "Assembler", TestAssemblerPackage.suite() );
+        addTest(ts,  null, com.hp.hpl.jena.rdf.arp.test.TestPackage.suite());
+        addTest(ts,  "Vocabularies", com.hp.hpl.jena.vocabulary.test.TestVocabularies.suite() );
+        addTest(ts,  null, com.hp.hpl.jena.shared.test.TestSharedPackage.suite() );
+        addTest(ts,  "Reasoners", com.hp.hpl.jena.reasoner.test.TestPackage.suite());
+        addTest(ts,  "Composed graphs", com.hp.hpl.jena.graph.compose.test.TestPackage.suite() );
+        addTest(ts,  "Ontology", com.hp.hpl.jena.ontology.impl.test.TestPackage.suite() );
+        addTest(ts,  "cmd line utils", jena.test.TestPackage.suite() );
+        return ts ;
     }
 
-    private void addTest(String name, TestSuite tc) {
-        tc.setName(name);
-        addTest(tc);
+    private static void addTest(TestSuite ts, String name, TestSuite tc) {
+        if ( name != null )
+            tc.setName(name);
+        ts.addTest(tc);
     }
+
 }
 
 /*
