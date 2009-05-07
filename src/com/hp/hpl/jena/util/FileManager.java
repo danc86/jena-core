580eafd3b3facdcb8cfe894409970805e10ca207
Andy Seaborne <andy.seaborne@gmail.com>
2009-05-07 20:17:38 +0000

    Fix reading of a remote URL so that it goes down th econtent negotiation path for HTTP URL's with no specific syntax or basename.

--- src/com/hp/hpl/jena/util/FileManager.java
+++ src/com/hp/hpl/jena/util/FileManager.java
@@ -294,7 +294,7 @@ public class FileManager
                 log.debug("Model cache hit: "+filenameOrURI) ;
             return modelCache.get(filenameOrURI) ;
         }
-        
+
         Model m = ModelFactory.createDefaultModel() ;
         readModelWorker(m, filenameOrURI, baseURI, rdfSyntax) ;
         
@@ -354,14 +354,18 @@ public class FileManager
     
     private Model readModelWorker(Model model, String filenameOrURI, String baseURI, String syntax)
     {
-        if ( baseURI == null )
-            baseURI = chooseBaseURI(filenameOrURI) ;
-
         // Doesn't call open() - we want to make the synatx guess based on the mapped URI.
         String mappedURI = mapURI(filenameOrURI) ;
 
         if ( log.isDebugEnabled() && ! mappedURI.equals(filenameOrURI) )
             log.debug("Map: "+filenameOrURI+" => "+mappedURI) ;
+
+        if ( syntax == null && baseURI == null && mappedURI.startsWith( "http:" ) )
+        {
+            // No syntax, no baseURI, HTTP URL ==> use content negotiation
+            model.read(mappedURI) ;
+            return model ;
+        }
         
         if ( syntax == null )
         {
@@ -371,6 +375,9 @@ public class FileManager
             if ( log.isDebugEnabled() ) 
                 log.debug("Syntax guess: "+syntax);
         }
+
+        if ( baseURI == null )
+            baseURI = chooseBaseURI(filenameOrURI) ;
 
         TypedStream in = openNoMapOrNull(mappedURI) ;
         if ( in == null )
@@ -384,8 +391,6 @@ public class FileManager
             // XXX
             //syntax
         }
-        
-        
         model.read(in.getInput(), baseURI, syntax) ;
         try { in.getInput().close(); } catch (IOException ex) {}
         return model ;
