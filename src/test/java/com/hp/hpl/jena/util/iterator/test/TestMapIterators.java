0202f6836cb526ac732d3c0ee3850077c083a64a
Chris Dollin <ehog.hedge@googlemail.com>
2009-09-28 11:17:34 +0000

    Adding tests for Map iterator

---
+++ src/test/java/com/hp/hpl/jena/util/iterator/test/TestMapIterators.java
@@ -0,0 +1,19 @@
+/*
+ 	(c) Copyright 2009 Hewlett-Packard Development Company, LP
+ 	All rights reserved.
+ 	$Id$
+*/
+
+package com.hp.hpl.jena.util.iterator.test;
+
+import com.hp.hpl.jena.rdf.model.test.ModelTestBase;
+
+public class TestMapIterators extends ModelTestBase
+    {
+    public TestMapIterators( String name )
+        { super( name ); }
+
+    public void testMe()
+        { fail( "SPOO" ); }
+    }
+
