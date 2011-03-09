8667b2710ec98cc9ea219dd9f8905feb6602705f
Ian Dickinson <i.j.dickinson@gmail.com>
2011-03-09 10:18:42 +0000

    Disabled test restored following suggestion from jena-dev to use .invalid TLD

--- src/test/java/com/hp/hpl/jena/ontology/impl/TestOntDocumentManager.java
+++ src/test/java/com/hp/hpl/jena/ontology/impl/TestOntDocumentManager.java
@@ -483,23 +483,25 @@ public class TestOntDocumentManager
     }
 
     /**
-     * Test disabled 9 Mar 2011. The "obviously won't resolve" URI http://example.org/not/exist
-     * now resolves to an IANA domain, thus breaking the test.
+     * Test the read-fail handler hook.
+     * Test updated to use the domain "example.invalid", not example.com, since .invalid
+     * is designed for domain names that are sure to be invalid. See
+     * <a href="http://tools.ietf.org/html/rfc2606#section-2">tools.ietf.org/html/rfc2606#section-2</a>
      */
-    public void xx_testReadFailHandler1() {
-//        OntDocumentManager o1 = new OntDocumentManager( "file:etc/ont-policy-test.rdf" );
-//
-//        TestFailHandler rfh = new TestFailHandler();
-//        o1.setReadFailureHandler( rfh );
-//
-//        // trigger the odm to read a non-existant source
-//        String source = "@prefix owl: <http://www.w3.org/2002/07/owl#> . <> a owl:Ontology ; owl:imports <http://example.com/not/exist>. ";
-//        OntModelSpec spec = new OntModelSpec( OntModelSpec.OWL_MEM );
-//        spec.setDocumentManager(  o1 );
-//        OntModel m = ModelFactory.createOntologyModel( spec );
-//        m.read( new StringReader( source ), "http://example.com/foo#", "N3" );
-//
-//        assertTrue( rfh.m_seen );
+    public void testReadFailHandler1() {
+        OntDocumentManager o1 = new OntDocumentManager( "file:etc/ont-policy-test.rdf" );
+
+        TestFailHandler rfh = new TestFailHandler();
+        o1.setReadFailureHandler( rfh );
+
+        // trigger the odm to read a non-existant source
+        String source = "@prefix owl: <http://www.w3.org/2002/07/owl#> . <> a owl:Ontology ; owl:imports <http://example.invalid/not/exist>. ";
+        OntModelSpec spec = new OntModelSpec( OntModelSpec.OWL_MEM );
+        spec.setDocumentManager(  o1 );
+        OntModel m = ModelFactory.createOntologyModel( spec );
+        m.read( new StringReader( source ), "http://example.com/foo#", "N3" );
+
+        assertTrue( rfh.m_seen );
     }
 
     public void testReadHook0() {
